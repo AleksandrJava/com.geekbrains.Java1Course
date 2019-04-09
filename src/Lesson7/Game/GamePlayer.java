84877c403520a8e89e758193bf387c941e9a9271
AleksandrJava <44460921+AleksandrJava@users.noreply.github.com>
2019-04-09 22:15:19 +0400

    add Lesson7

---
+++ src/Lesson7/Game/GamePlayer.java
@@ -0,0 +1,16 @@
+package Lesson7.Game;
+
+public class GamePlayer {
+    private char playerSign;
+
+
+    public GamePlayer(char playerSign){
+        this.playerSign = playerSign;
+    }
+
+
+
+    public char getPlayerSign(){
+        return playerSign;
+    }
+}
---
+++ src/Lesson7/MainClass.java
@@ -0,0 +1,11 @@
+package Lesson7;
+
+import Lesson7.Game.Game;
+
+public class MainClass {
+    public static void main(String[] args){
+        Game gameInstance = new Game();
+        gameInstance.initGame();
+    }
+
+}
